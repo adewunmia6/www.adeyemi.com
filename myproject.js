185abfd653f3384db3dae39c52fceb6baa3379b2
adewunmia6 <83634153+adewunmia6@users.noreply.github.com>
2021-05-04 08:29:44 +0100

    Add files via upload

---
+++ myproject.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
